1b8d36e68ba3e38bd510b6c1515a963c2be04e40
Martin Šimůnek <public@e-mis.cz>
2023-05-25 20:55:23 +0200

    Opraveno vytváření flower v main()
    Chyběla velikost

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Flower flower = new Flower();
+        Flower flower = new Flower(5);
         System.out.println("Hello world!");
         // Pokus
         System.out.println("Dnes je ale krásně! ;)");
