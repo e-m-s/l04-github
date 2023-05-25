c1d10eb456a768e5d1be446aaca3d9e3cf7525e9
Martin <public@e-mis.cz>
2023-05-25 20:04:17 +0200

    Vylepšení povzbuzujícího hlášení
    Zní ještě více nadšeně.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         // Pokus
-        System.out.println("Dnes je ale hezky! ;)");
+        System.out.println("Dnes je ale krásně! ;)");
     }
 }
