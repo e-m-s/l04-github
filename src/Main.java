04f664167a54f3f07c715a349a4b1af43fb3efac
Martin Šimůnek <public@e-mis.cz>
2023-05-25 20:33:52 +0200

    Přidána třída Flower a vytvořena instance
    Instanci vytváříme v main().

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        Flower flower = new Flower();
         System.out.println("Hello world!");
         // Pokus
         System.out.println("Dnes je ale krásně! ;)");
